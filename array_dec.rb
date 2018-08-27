a8296667227c302d6dea9e9e11dc29b15fb6b49a
Muhammad Tahir <tahirror@gmail.com>
2018-08-27 09:29:53 -0400

    testing

---
+++ array_dec.rb
@@ -0,0 +1,15 @@
+arr = Array.new([[0,0,1,0,0],
+                 [1,0,0,1,0],
+                 [1,0,0,0,0],
+                 [1,1,0,1,0],
+                 [0,1,0,0,1]])
+
+arra = Array.new([5,6,3,2,1])
+
+arr.each do |row|
+  row.sort!
+end
+
+puts arr
+
+puts arra
