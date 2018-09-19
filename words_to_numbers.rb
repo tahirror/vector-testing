a91c6e5630d055ffcd2f4a84e94d399202651f35
Muhammad Tahir <tahirror@gmail.com>
2018-09-19 11:50:26 -0400

    words to number

--- words_to_numbers.rb
+++ words_to_numbers.rb
@@ -2,7 +2,7 @@ require 'numbers_in_words'
 
 words_arr = ['zero', 'one hundred', 'three googol', 'zero', 'nine', 'six', 'zero', 'null', 'zero', 'one', 'three', 'zero', 'nine', 'six', 'zero', 'null', 'zero', 'one', 'three', 'zero', 'nine', 'six', 'zero', 'null']
 
-def words_to_int(words_arr)
+def words_to_int_str(words_arr)
   output=''
   words_arr.each do |w|
     output += "#{NumbersInWords.in_numbers(w)}"
@@ -10,4 +10,4 @@ def words_to_int(words_arr)
   output
 end
 
-puts "Result: #{words_to_int(words_arr)}"
+puts "Result: #{words_to_int_str(words_arr)}"
